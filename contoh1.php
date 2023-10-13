47721e8e7ed89dfd8cbe0366eb925f6f03b729e6
syifaaulia123 <145749462+syifaaulia123@users.noreply.github.com>
2023-10-13 11:29:17 +0700

    Add files via upload

--- contoh1.php
+++ contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index() 
     { 
         echo "<h1>Perkenalkan</h1>"; 
-        echo"Nama saya Imam Nawawi 
-        Saya tingga di daerah Ciputat 
+        echo"Nama saya Imam Nawawi <br> 
+        Saya tingga di daerah Ciputat <br> 
         olah raga yang saya sukai adalah 
         Bulutangkis"; 
     } 
